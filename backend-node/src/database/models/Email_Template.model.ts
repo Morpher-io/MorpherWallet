30efb8b8002d7d40cfcde8ac2e22856ee041ebd5
Beorn Bishop <mailto@beornb.com>
2021-09-07 17:28:16 +0200

    Email Template

---
+++ backend-node/src/database/models/Email_Template.model.ts
@@ -0,0 +1,36 @@
+import { AutoIncrement, Column, DataType, ForeignKey, Model, PrimaryKey, Table, Unique } from 'sequelize-typescript';
+import { User } from './User.model';
+
+@Table({ timestamps: true })
+export class Email_Template extends Model {
+    @PrimaryKey
+    @AutoIncrement
+    @Column
+    id: number;
+
+    @Column({
+        type: DataType.STRING
+    })
+    template_name;
+
+    @Column({
+        type: DataType.STRING
+    })
+    from_address;    
+
+    @Column({
+        type: DataType.STRING
+    })
+    subject;        
+
+    @Column({
+        type: DataType.TEXT
+    })
+    template_html;
+
+    @Column({
+        type: DataType.TEXT
+    })
+    template_text;
+
+}
