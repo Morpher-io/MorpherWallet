1ea0b462130188f89a177cd16b13218dcb319256
Malkky <arjeti.kraja@gmail.com>
2021-09-21 17:02:39 +0200

    Disabled pupeteer for now

--- backend-node/src/tests/recoveries.test.ts
+++ backend-node/src/tests/recoveries.test.ts
@@ -11,27 +11,27 @@ const devWalletPassword = process.env.DEV_WALLET_PASSWORD;
 
 describe('Recovery methods test cases', async () => {
     it('testing', async () => {
-        const browser = await puppeteer.launch(  { headless: false });
-        const page = await browser.newPage();
-        await page.goto(`https://${auth}@wallet-dev.morpher.com`);
-
-
-        await page.focus('[data-cy=walletEmail]');
-        await page.keyboard.type(devWalletEmail);
-
-        await page.focus('[data-cy=walletPassword]');
-        await page.keyboard.type(devWalletPassword);
-
-        await page.click('[data-cy=submit]');
-
-        await page.waitForSelector('[data-cy=currentEmail]');
-
-        await page.click('[data-cy=settingsButton]');
-
-        console.log(await page.$('.settings-link'))
-
-        await page.screenshot({ path: 'example.png' });
-
-        await browser.close();
+        // const browser = await puppeteer.launch(  { headless: false });
+        // const page = await browser.newPage();
+        // await page.goto(`https://${auth}@wallet-dev.morpher.com`);
+        //
+        //
+        // await page.focus('[data-cy=walletEmail]');
+        // await page.keyboard.type(devWalletEmail);
+        //
+        // await page.focus('[data-cy=walletPassword]');
+        // await page.keyboard.type(devWalletPassword);
+        //
+        // await page.click('[data-cy=submit]');
+        //
+        // await page.waitForSelector('[data-cy=currentEmail]');
+        //
+        // await page.click('[data-cy=settingsButton]');
+        //
+        // console.log(await page.$('.settings-link'))
+        //
+        // await page.screenshot({ path: 'example.png' });
+        //
+        // await browser.close();
     })
 })
