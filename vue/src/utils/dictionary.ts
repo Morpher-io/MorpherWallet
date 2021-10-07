d2ea4088e4a6693c3cc73a7b3967407d982d3d62
Beorn Bishop <beorn@morpher.com>
2021-10-07 12:11:37 +0200

    cypress fixes

--- vue/src/utils/dictionary.ts
+++ vue/src/utils/dictionary.ts
@@ -1,6 +1,10 @@
 import { i18n } from '@/plugins/i18n';
 
 const getDictionaryValue = (key: string): string => {
+	
+	if (!key)
+	  return i18n.t('errors.FALLBACK_MESSAGE').toString();
+	  
 	let message: string = i18n.t('errors.' + key).toString();
 
 	if (!message) {
