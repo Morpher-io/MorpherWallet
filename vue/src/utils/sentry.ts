3a4360e9e03026a0577682c4e86b34e6f2bc25f2
Beorn Bishop <beorn@morpher.com>
2022-01-19 11:01:50 +0100

    Sentry Error Props

---
+++ vue/src/utils/sentry.ts
@@ -0,0 +1,22 @@
+const errorFilter = [
+	'User denied account access',
+	'User denied transaction',
+	'NavigationDuplicated',
+	'ACCESS_DENIED',
+	'Missing credentials in config',
+	'Response not successful: Received status code 400',
+	'Network error: Failed to fetch',
+	'400 (Bad Request)'
+];
+
+/*
+ * used to filter out known errors so that they are not logged to sentry
+ */
+export const checkErrorFilter = (error: string): boolean => {
+	const filterErr = errorFilter.find(filter => error.toLowerCase().includes(filter.toLowerCase()));
+	if (filterErr) {
+		return false;
+	} else {
+		return true;
+	}
+};
