5cb37749d2ff095e272a520faf8c5ab4de1b8fb5
Thomas Wiesner (thomaswiesner.com) <485781+tomw1808@users.noreply.github.com>
2023-08-03 15:38:39 +0200

    replaced frontend by git submodule

--- vue/src/lang/index.ts
+++
@@ -1,9 +0,0 @@
-import en from './locales/en.json';
-import bs from './locales/bs.json';
-import ru from './locales/ru.json';
-
-export default {
-	en,
-	bs,
-	ru
-};
--- vue/src/types/helpers.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'eth-lightwallet' {
-	const Lightwallet: any;
-	export default Lightwallet;
-}
-
-declare module 'vue-password-strength-meter' {
-	const Password: any;
-	export default Password;
-}
