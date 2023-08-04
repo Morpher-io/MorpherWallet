cc862db269ee3511127a9211d86cb67cff6d75ea
Thomas Wiesner (thomaswiesner.com) <485781+tomw1808@users.noreply.github.com>
2023-08-04 09:02:26 +0200

    added backend-node as submodule

--- backend-node/src/database/models/index.ts
+++
@@ -1,8 +0,0 @@
-
-export { Recovery } from './Recovery.model';
-export { Recovery_Type } from './Recovery_Type.model';
-export { User } from './User.model';
-export { Userhistory } from './Userhistory.model';
-export { Email_Template } from './Email_Template.model';
-export { Email_Log } from './Email_Log.model';
-
